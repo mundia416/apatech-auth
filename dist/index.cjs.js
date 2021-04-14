45cc7f96e4d166533d5cfe391d6be12581d4c311
Mundia <fxmundia416@gmail.com>
2021-04-14 09:56:21 +0200

    initial commit

---
+++ dist/index.cjs.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var context = require("./context");
+
+var userResolvers = require("./resolvers/userResolver");
+
+var secondaryUserResolvers = require("./resolvers/secondaryUserResolver");
+
+var userTypeDef = require("./typedefs/user");
+/**
+ * to use this library do the following
+ */
+//define the following variables in the env file
+
+/**
+ 
+SENDGRID_API_KEY=
+//the email address that password reset emails are sent from
+SENDGRID_FROM_EMAIL=
+//the url of the site
+BASE_URL=
+
+ */
+
+
+module.exports = {
+  //1. add the context object to ApolloServer object
+  context: context,
+  //2. add the userResolver to the resolvers index
+  userResolvers: userResolvers,
+  //3. add the userTypeDef to the typedefs index
+  userTypeDef: userTypeDef,
+  //4.[optional] add the secondaryUserResolver to the resolvers index if using secondary logins
+  secondaryUserResolvers: secondaryUserResolvers
+};
---
+++ index.js
@@ -0,0 +1,32 @@
+const context = require("./context");
+const userResolvers = require("./resolvers/userResolver");
+const secondaryUserResolvers = require("./resolvers/secondaryUserResolver");
+const userTypeDef = require("./typedefs/user");
+
+
+
+/**
+ * to use this library do the following
+ */
+
+//define the following variables in the env file
+/**
+ 
+SENDGRID_API_KEY=
+//the email address that password reset emails are sent from
+SENDGRID_FROM_EMAIL=
+//the url of the site
+BASE_URL=
+
+ */
+
+module.exports = {
+    //1. add the context object to ApolloServer object
+    context,
+    //2. add the userResolver to the resolvers index
+    userResolvers,
+    //3. add the userTypeDef to the typedefs index
+    userTypeDef,
+    //4.[optional] add the secondaryUserResolver to the resolvers index if using secondary logins
+    secondaryUserResolvers,
+}
